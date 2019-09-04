a44924b75be66146f7f7e09098fde4516872e701
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-09-04 14:56:20 -0500

    Add my hello world monogame experiment

---
+++ Experiments/Daniel/test2/DanielExperiment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using test2;
+
+namespace test
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
