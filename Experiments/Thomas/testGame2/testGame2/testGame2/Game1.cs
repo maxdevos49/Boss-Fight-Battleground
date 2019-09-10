846fd98942429b0435d88333207af5007acac2d7
Thomas Powell <tkpowell@iastate.edu>
2019-09-10 09:14:11 -0500

    Added background

--- Experiments/Thomas/testGame2/testGame2/testGame2/Game1.cs
+++ Experiments/Thomas/testGame2/testGame2/testGame2/Game1.cs
@@ -4,13 +4,13 @@ using Microsoft.Xna.Framework.Input;
 
 namespace testGame2
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        Texture2D background_Sprite;
+        Texture2D player_Sprite;
 
         public Game1()
         {
@@ -18,12 +18,7 @@ namespace testGame2
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -31,22 +26,17 @@ namespace testGame2
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            background_Sprite = Content.Load<Texture2D>("background");
+            player_Sprite = Content.Load<Texture2D>("player");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
@@ -75,7 +65,11 @@ namespace testGame2
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(background_Sprite, new Vector2(0, 0), Color.White);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
