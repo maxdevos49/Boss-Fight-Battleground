2c3cab81e29f1823b6c5ab6c9736787cb6cb78ed
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-09-10 14:31:36 -0500

    Update dependencies for tests and change player unit test

--- Experiments/Daniel/TestGameWithTests/TestGame.Tests/PlayerTests.cs
+++ Experiments/Daniel/TestGameWithTests/TestGame.Tests/PlayerTests.cs
@@ -8,23 +8,13 @@ namespace TestGame.Tests
     public class PlayerTests
     {
         [Fact]
-        public void updatePosition()
+        public void ConstructorTest()
         {
-            Vector2 dimensions = new Vector2(1, 1);
-            Player myPlayer = new Player("", new Vector2(0,0), new Vector2(0,0));
-            String expected = "This should always pass!";
-            String actual = "This should always pass!";
+            Player myPlayer = new Player("2d\\stickman-test-2", new Vector2(0,0), new Vector2(0,0));
 
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Test2()
-        {
-            String expected = "This should always pass!";
-            String actual = "Ha! Just kidding!";
-
-            Assert.Equal(expected, actual);
+            Assert.Equal(20, myPlayer.health);
+            Assert.Equal(new Vector2(0), myPlayer.velocity);
+            Assert.True(myPlayer.isInAir);
         }
     }
 }
