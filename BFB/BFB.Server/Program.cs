580517836e81ba8a1682dc2598fb2a43bda97ce8
maxdevos49 <maxdevos49@gmail.com>
2019-09-11 23:05:16 -0500

    renamed some things and added the pipeline project for processing our raw images and content

---
+++ BFB/BFB.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BFB.GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
