04f9bf67edd2d8abe084ecb79a4b6b7b4f4e2459
Thomas Powell <tkpowell@iastate.edu>
2019-09-17 14:06:23 -0500

    Started chunk class.

--- BFB/BFB.Engine/TileMap/Chunk.cs
+++ BFB/BFB.Engine/TileMap/Chunk.cs
@@ -6,5 +6,39 @@ namespace BFB.Engine.TileMap
 {
     class Chunk
     {
+        private const int CHUNKSIZE = 16;
+
+        private int[,] hardness = new int[CHUNKSIZE, CHUNKSIZE];
+
+        private int[,] light = new int[CHUNKSIZE, CHUNKSIZE];
+
+        private int[,] wall = new int[CHUNKSIZE, CHUNKSIZE];
+
+        private int[,] block = new int[CHUNKSIZE, CHUNKSIZE];
+
+        public Chunk()
+        {
+
+        }
+
+        public int getHardness(int x, int y)
+        {
+            return hardness[x, y];
+        }
+
+        public int getLight(int x, int y)
+        {
+            return light[x, y];
+        }
+
+        public int getWall(int x, int y)
+        {
+            return wall[x, y];
+        }
+
+        public int getBlock(int x, int y)
+        {
+            return block[x, y];
+        }
     }
 }
