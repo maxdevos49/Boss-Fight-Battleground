0ee2e34f6d2b4df8ee427fec8f5e2d15132da77e
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-05 14:38:12 -0500

    Change name from BFB.WebServer to BFB (long) and add a Register page

--- BFB/BFB.Web/Startup.cs
+++ BFB/BFB.Web/Startup.cs
@@ -31,8 +31,8 @@ namespace BFB.Web
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
 
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
