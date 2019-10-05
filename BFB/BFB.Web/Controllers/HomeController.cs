0cfa7e1662f33ddef39cde55f6677ad825811c83
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-05 14:38:12 -0500

    Change name from BFB.WebServer to BFB (long) and add a Register page

--- BFB/BFB.Web/Controllers/HomeController.cs
+++ BFB/BFB.Web/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace BFB.WebServer.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
