4afede2a27328fa654f9029e0faf92c99d79d273
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-16 18:31:05 -0500

    Store database connection string in appsettings

--- BFB/BFB.Web/Startup.cs
+++ BFB/BFB.Web/Startup.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BFB.Web.Controllers;
+using BFB.Web.Models;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +36,8 @@ namespace BFB.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
 
             });
+
+            services.Configure<DatabaseConfig>(Configuration.GetSection("BFBDatabase"));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
