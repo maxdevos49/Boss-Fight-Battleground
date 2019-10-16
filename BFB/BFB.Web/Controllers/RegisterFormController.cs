5591f797b7705b0ef536f66980c328de8ba70d96
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-16 17:22:45 -0500

    Added RegisterForm controller/model

---
+++ BFB/BFB.Web/Controllers/RegisterFormController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BFB.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BFB.Web.Controllers
+{
+    public class RegisterFormController : Controller
+    {
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterFormModel model)
+        {
+            return Content($"Hello {model.Username}");
+        }
+    }
+}
