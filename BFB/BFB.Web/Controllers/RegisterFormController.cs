dae633a6f77053c52fa47eaf31d1dceb225d723a
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-16 18:31:05 -0500

    Store database connection string in appsettings

--- BFB/BFB.Web/Controllers/RegisterFormController.cs
+++ BFB/BFB.Web/Controllers/RegisterFormController.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security;
 using System.Threading.Tasks;
 using BFB.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace BFB.Web.Controllers
 {
     public class RegisterFormController : Controller
     {
+
+        private string connString;
+
+        public RegisterFormController(IOptions<DatabaseConfig> config)
+        {
+            connString = config.Value.ConnectionString;
+        }
+
         [HttpPost, ValidateAntiForgeryToken]
         public IActionResult Register(RegisterFormModel model)
         {
+            using (SqlConnection db = new SqlConnection(connString))
+            {
+
+                db.Open();
+            }
             return Content($"Hello {model.Username}");
         }
     }
---
+++ BFB/BFB.Web/Models/DatabaseConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BFB.Web.Models
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
