7246d22ffb179b5db5fa5f0e4fbec48d04c01d16
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-20 15:22:07 -0500

    Abstract adding code to a service

--- BFB/BFB.Web/Controllers/RegisterFormController.cs
+++ BFB/BFB.Web/Controllers/RegisterFormController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security;
 using System.Threading.Tasks;
 using BFB.Web.Models;
+using BFB.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using MySql.Data.MySqlClient;
@@ -16,21 +17,7 @@ namespace BFB.Web.Controllers
         [HttpPost, ValidateAntiForgeryToken]
         public IActionResult Register(RegisterFormModel model, [FromServices] DatabaseConfig db)
         {
-            string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
-            db.BFB_User.Add(new BFB_User
-            {
-                Username = model.Username,
-                Email = model.Email,
-                Password = model.Password,
-                IsVerified = false,
-                IsBanned = false,
-                IsActive = false,
-                InsertedOn = currentDate,
-                UpdatedOn = currentDate,
-                UpdatedBy = currentDate,
-                EmailToken = null
-            });
-            db.SaveChanges();
+            DatabaseService.AddUserEntry(db, model.Username, model.Email, model.Password, DateTime.Now);
             return Content($"Hello {model.Username}");
         }
     }
--- BFB/BFB.Web/Services/DatabaseService.cs
+++ BFB/BFB.Web/Services/DatabaseService.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BFB.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BFB.Web.Services
 {
     public static class DatabaseService
     {
-        public static void AddEntry()
+        // Add a user to BFB_User table
+        public static void AddUserEntry(DatabaseConfig db, string username, string email, string password, DateTime currentTime)
         {
-
+            string currentDate = currentTime.ToString("yyyy-MM-dd");
+            db.BFB_User.Add(new BFB_User
+            {
+                Username = username,
+                Email = email,
+                Password = password,
+                IsVerified = false,
+                IsBanned = false,
+                IsActive = false,
+                InsertedOn = currentDate,
+                UpdatedOn = currentDate,
+                UpdatedBy = currentDate,
+                EmailToken = null
+            });
+            db.SaveChanges();
         }
     }
 }
