3b0de9763ae1ae58412993ff9bb814d61dcc573f
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-22 12:58:00 -0500

    Add more tests

--- BFB/BFB.Test/Web/Database/DatabaseService.spec.cs
+++ BFB/BFB.Test/Web/Database/DatabaseService.spec.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using BFB.Web.Models;
 using Xunit;
@@ -37,6 +38,32 @@ namespace BFB.Test.Web.Database
             var mock = new Mock<DatabaseConfig>();
             mock.Setup(foo => foo.BFB_User.Add(fakeUser)).Verifiable();
             //BFB.Web.Services.DatabaseService.AddUserEntry(username, email, password);
+        }
+
+        [Fact]
+        public void ValidateUserTest()
+        {
+            string username = "test";
+            string password = "test2";
+            string email = "test@test.test";
+            DateTime currentDate = DateTime.Now;
+
+            BFB_User fakeUser = new BFB_User
+            {
+                Username = username,
+                Email = email,
+                Password = password,
+                IsVerified = false,
+                IsBanned = false,
+                IsActive = false,
+                InsertedOn = currentDate,
+                UpdatedOn = currentDate,
+                UpdatedBy = currentDate,
+                EmailToken = null
+            };
+            var mock = new Mock<DatabaseConfig>();
+            mock.Setup(foo => foo.BFB_User.Add(fakeUser));
+            //Assert.True(BFB.Web.Services.DatabaseService.ValidateUser(username, password));
         }
 
         #endregion
