f01df0e29c7840fee452b32bc40fb0b5505e5a5b
Mason Timmerman <masonlt199@gmail.com>
2019-10-23 22:28:54 -0500

    scrapped what I had, cant get it to work

--- BFB/BFB.Engine/Camera/Camera.cs
+++ BFB/BFB.Engine/Camera/Camera.cs
@@ -1,14 +1,12 @@
+using BFB.Engine.Math;
 using Microsoft.Xna.Framework;
 
 namespace BFB.Engine.Camera
 {
     public class Camera
     {
-        private Vector2 _playerPos;
-        
-        public Camera(Vector2 playerPos)
+        public Camera()
         {
-            _playerPos = playerPos;
         }
     }
 }
