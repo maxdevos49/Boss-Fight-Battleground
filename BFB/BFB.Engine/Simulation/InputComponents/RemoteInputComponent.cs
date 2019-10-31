dfeb4834aa08006cbdc03cae21c41999b46380cf
maxdevos49 <maxdevos49@gmail.com>
2019-10-31 16:34:43 -0500

    commiting changes

--- BFB/BFB.Engine/Simulation/InputComponents/RemoteInputComponent.cs
+++ BFB/BFB.Engine/Simulation/InputComponents/RemoteInputComponent.cs
@@ -17,6 +17,7 @@ namespace BFB.Engine.Simulation.InputComponents
         
         #endregion
 
+        
         public RemoteInputComponent(ClientSocket socket)
         {
             _lock = new object();
