522d3adfcf74bbcd773ad2480582756e93e0df36
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-11-04 01:48:31 -0600

    Finish documenting content

--- BFB/BFB.Engine/Content/AnimationState.cs
+++ BFB/BFB.Engine/Content/AnimationState.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace BFB.Engine.Content
 {
+    /// <summary>
+    /// The animation state, decided by the server, so it is serializable. The client then learns what the animation state is and figures out how to get that from the spritesheet.
+    /// </summary>
     [Serializable]
     public enum AnimationState
     {
