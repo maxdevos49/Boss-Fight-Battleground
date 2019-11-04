ee6af5c8425560e9c389b75ba84ee6002e816d60
Thomas Powell <tksp98@gmail.com>
2019-11-04 16:42:46 -0600

    Started TileComponent documentation

--- BFB/BFB.Engine/TileMap/TileComponent/ITileComponent.cs
+++ BFB/BFB.Engine/TileMap/TileComponent/ITileComponent.cs
@@ -1,5 +1,8 @@
 namespace BFB.Engine.TileMap.TileComponent
 {
+    /// <summary>
+    /// The interface for the 
+    /// </summary>
     public interface ITileComponent
     {
          void TickTile(WorldManager worldManager,Chunk chunk, int blockX, int blockY);
