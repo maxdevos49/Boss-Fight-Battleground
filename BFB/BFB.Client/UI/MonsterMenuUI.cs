8cd8c88020a67ea23e5423a75a70c91246ccfa14
Thomas Powell <tksp98@gmail.com>
2019-11-17 11:47:02 -0600

    Added monster selection UI

---
+++ BFB/BFB.Client/UI/MonsterMenuUI.cs
@@ -0,0 +1,87 @@
+﻿using BFB.Client.Scenes;
+using BFB.Engine.UI;
+using BFB.Engine.UI.Components;
+using BFB.Engine.UI.Constraints;
+using Microsoft.Xna.Framework;
+
+namespace BFB.Client.UI
+{
+    public class MonsterMenuUI : UILayer
+    {
+        public MonsterMenuUI() : base(nameof(GameMenuUI)) { }
+
+        public override void Body()
+        {
+            RootUI.Hstack(h1 =>
+            {
+
+                h1.Spacer();
+
+                h1.Vstack(v1 =>
+                {
+
+                    v1.Spacer();
+
+                    v1.Hstack(h2 =>
+                    {
+                        h2.Hstack(h3 => { h3.Text("Select Your Monster"); })
+                            .Height(0.7f)
+                            .Width(0.7f)
+                            .Center();
+                    })
+                        .Grow(3);
+
+                    v1.Hstack(h2 =>
+                    {
+                        h2.Button("Spider",
+                                clickAction: (e, a) => { SceneManager.StartScene(nameof(HudUI)); })
+                            .Height(0.8f)
+                            .Width(0.8f)
+                            .Image("button")
+                            .Center();
+                    });
+
+                    v1.Hstack(h2 =>
+                    {
+                        h2.Button("Zombie",
+                                clickAction: (e, a) => { UIManager.Start(nameof(HudUI)); })
+                            .Height(0.8f)
+                            .Width(0.8f)
+                            .Image("button")
+                            .Center();
+                    });
+
+                    v1.Hstack(h2 =>
+                    {
+                        h2.Button("Skeleton",
+                                clickAction: (e, a) => { UIManager.Start(nameof(HudUI)); })
+                            .Height(0.8f)
+                            .Width(0.8f)
+                            .Image("button")
+                            .Center();
+                    });
+
+                    v1.Hstack(h2 =>
+                    {
+                        h2.Button("The BOSS",
+                                clickAction: (e, a) => { UIManager.Start(nameof(HudUI)); })
+                            .Height(0.8f)
+                            .Width(0.8f)
+                            .Image("button")
+                            .Center();
+                    });
+
+                    v1.Spacer(3);
+
+                })
+                    .Grow(3);
+
+                h1.Spacer();
+
+            })
+                .Background(new Color(0, 0, 0, 100));
+        }
+    }
+
+
+}
